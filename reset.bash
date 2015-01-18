d58a75cfa4bfe8f7ba88a61f21352e1c892f7595
Joe Heck <heckj@mac.com>
2015-01-18 13:05:54 -0800

    running processes within screen

---
+++ reset.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+# FOG
+cd ~/src/renasar-fog-ui
+rm -rf node_modules
+rm -rf dist
+rm -rf bower_components
+npm install
+npm run build
+npm test
+# DHCP
+cd ~/src/renasar-dhcp
+rm -rf node_modules
+npm install
+# TFTP
+cd ~/src/renasar-tftp
+rm -rf node_modules
+npm install
+# HTTP
+cd ~/src/renasar-http
+rm -rf node_modules
+npm install
+npm run apidoc
+# SYSLOG
+cd ~/src/renasar-syslog
+rm -rf node_modules
+npm install
+# TASKGRAPH
+cd ~/src/renasar-taskgraph
+rm -rf node_modules
+npm install
