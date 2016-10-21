85e1bf3b85286034487462c717775c9350fa6c43
pengt2 <Tian.Peng@emc.com>
2016-10-21 11:53:45 +0800

    import common

--- manifest-build-tools/lib/RepositoryOperator.py
+++ manifest-build-tools/lib/RepositoryOperator.py
@@ -5,26 +5,7 @@ import os
 import config
 from gitbits import GitBit
 from ParallelTasks import ParallelTasks
-
-def strip_suffix(text, suffix):
-    """
-    Cut a set of the last characters from a provided string
-    :param text: Base string to cut
-    :param suffix: String to remove if found at the end of text
-    :return: text without the provided suffix
-    """
-    if text is not None and text.endswith(suffix):
-        return text[:len(text) - len(suffix)]
-    else:
-        return text
-
-
-def strip_prefix(text, prefix):
-    if text is not None and text.startswith(prefix):
-        return text[len(prefix):]
-    else:
-        return text
-
+from common import *
 
 class RepoCloner(ParallelTasks):
     """
