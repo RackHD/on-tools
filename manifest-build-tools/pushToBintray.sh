432fb2f56a87b0820e4e4a1a433e2c6c9591a146
pengt2 <Tian.Peng@emc.com>
2016-10-21 15:16:29 +0800

    get the directory of script resides

--- manifest-build-tools/pushToBintray.sh
+++ manifest-build-tools/pushToBintray.sh
@@ -4,9 +4,12 @@
 
 set -e
 
-BINTRAY_CONFIG_FILE=bintray.conf
+BASEDIR=$(dirname "$0")
+
+BINTRAY_CONFIG_FILE=$BASEDIR/bintray.conf
+
 if [ ! -f "$BINTRAY_CONFIG_FILE" ]; then
-    echo "The bintray.conf is missing, exiting now..."
+    echo "The bintray.conf is missing. Exiting now..."
     exit 1
 fi
 
