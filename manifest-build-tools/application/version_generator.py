acd9f4b36bc5e9ad0703ed004070998c21bf7cb6
pengtian0 <Tian.Peng@emc.com>
2016-12-01 16:25:14 +0800

    rework to align new version naming

--- manifest-build-tools/application/version_generator.py
+++ manifest-build-tools/application/version_generator.py
@@ -3,34 +3,36 @@
 
 """
 The script compute the version of a package, just like:
-1.1-1-devel-20160809150908-7396d91
+1.1-1-20161129UTC
 
 usage:
-./on-tools/manifest-build-tools/HWIMO-BUILD on-tools/manifest-build-tools/application/version_generator.py 
---repo-dir /home/onrack/rackhd/release/rackhd-repos/PengTian0/b/b/on-http
---manifest-repo-dir /home/onrack/rackhd/release/rackhd-repos/PengTian0/b/build-manifests
---is-official-release
+./on-tools/manifest-build-tools/HWIMO-BUILD on-tools/manifest-build-tools/application/version_generator.py \
+--repo-dir /home/onrack/rackhd/release/rackhd-repos/PengTian0/b/b/on-http \
+--is-official-release true
 
 Because this script need to import scripts under lib.
 The script HWIMO-BUILD helps to add the scripts under lib to python path.
 
 The required parameters: 
-repo-dir
-manifest-repo-dir
+repo-dir: the directory of the repository
 
 The optional parameters:
-is-official-release (default value is false)
+is-official-release: whether the release is official (default value is false)
 """
 import os
 import sys
 import argparse
-import datetime
-import subprocess
-from RepositoryOperator import RepoOperator
-from common import *
+from datetime import datetime,timedelta
+
+try:
+    from RepositoryOperator import RepoOperator
+    import common
+except ImportError as import_err:
+    print import_err
+    sys.exit(1)
 
 class VersionGenerator(object):
-    def __init__(self, repo_dir, manifest_repo_dir):
+    def __init__(self, repo_dir):
         """
         This module compute the version of a repository
         The version for candidate release: {big-version}~{version-stage}-{small-version}
@@ -40,21 +42,31 @@ class VersionGenerator(object):
         :return:None
         """
         self._repo_dir = repo_dir
-        self._manifest_repo_dir = manifest_repo_dir
         self.repo_operator = RepoOperator()
- 
+        self._repo_name = self.get_repo_name()
+
+    def get_repo_name(self):
+        repo_url = self.repo_operator.get_repo_url(self._repo_dir)
+        repo_name = common.strip_suffix(os.path.basename(repo_url), ".git")
+        return repo_name
+
     def generate_small_version(self):
         """
         Generate the small version which consists of commit date and commit hash of manifest repository
         According to small version, users can track the commit of all repositories in manifest file
         return: small version 
         """
-
-        ts_str = self.repo_operator.get_lastest_commit_date(self._manifest_repo_dir)
-        date = datetime.datetime.utcfromtimestamp(int(ts_str)).strftime('%Y%m%d%H%M%SZ')
-        commit_id = self.repo_operator.get_lastest_commit_id(self._manifest_repo_dir)
-        version = "{date}-{commit}".format(date=date, commit=commit_id[0:7])
-        return version
+        if self._repo_name == "RackHD":
+            utc_now = datetime.utcnow()
+            utc_yesterday = utc_now + timedelta(days=-1)
+            version = utc_yesterday.strftime('%Y%m%dUTC')
+            return version
+        else:
+            commit_timestamp_str = self.repo_operator.get_lastest_commit_date(self._repo_dir)
+            date = datetime.utcfromtimestamp(int(commit_timestamp_str)).strftime('%Y%m%d%H%M%SZ')
+            commit_id = self.repo_operator.get_lastest_commit_id(self._repo_dir)
+            version = "{date}-{commit}".format(date=date, commit=commit_id[0:7])
+            return version
 
     def debian_exist(self):
         """
@@ -74,8 +86,6 @@ class VersionGenerator(object):
         The big version is the latest version of debian/changelog
         return: big version
         """
-        repo_url = self.repo_operator.get_repo_url(self._repo_dir)
-        repo_name = strip_suffix(os.path.basename(repo_url), ".git")
         # If the repository has the debianstatic/repository name/,
         # create a soft link to debian before compute version
         debian_exist = self.debian_exist()
@@ -85,28 +95,20 @@ class VersionGenerator(object):
                 if filename == "debianstatic":
                     debianstatic_dir = os.path.join(self._repo_dir, "debianstatic")
                     for debianstatic_filename in os.listdir(debianstatic_dir):
-                        if debianstatic_filename == repo_name:
-                            debianstatic_repo_dir = "debianstatic/{0}".format(repo_name)
-                            link_dir(debianstatic_repo_dir, "debian", self._repo_dir)
+                        if debianstatic_filename == self._repo_name:
+                            debianstatic_repo_dir = "debianstatic/{0}".format(self._repo_name)
+                            common.link_dir(debianstatic_repo_dir, "debian", self._repo_dir)
                             linked = True
         if not debian_exist and not linked:
             return None
         cmd_args = ["dpkg-parsechangelog", "--show-field", "Version"]
-        proc = subprocess.Popen(cmd_args,
-                                cwd=self._repo_dir,
-                                stderr=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                shell=False)
-        (out, err) = proc.communicate()
+        version = common.run_command(cmd_args, directory=self._repo_dir)
 
         if linked:
             os.remove(os.path.join(self._repo_dir, "debian"))
 
-        if proc.returncode == 0:
-            return out.strip()
-        else:
-            raise RuntimeError("Failed to parse version in debian/changelog due to {0}".format(err))
-        
+        return version
+
     def generate_version_stage(self):
         """
         Generate the version stage according to the stage of deveplopment
@@ -129,19 +131,17 @@ class VersionGenerator(object):
         """
         big_version = self.generate_big_version()
         if big_version is None:
-            logging.warning("Failed to generate big version, maybe the {0} doesn't contain debian directory".format(self._repo_dir))
+            common.logging.warning("Failed to generate big version, maybe the {0} doesn't contain debian directory".format(self._repo_dir))
             return None
 
         if is_official_release:
             version = big_version
         else:
-            version_stage = self.generate_version_stage()
             small_version = self.generate_small_version()
+            if small_version is None:
+                raise RuntimeError("Failed to generate version for {0}, due to the small version is None".format(self._repo_dir))
 
-            if version_stage is None or small_version is None:
-                raise RuntimeError("Failed to generate version for {0}, due to the candidate version or small version is None".format(self._repo_dir))
-
-            version = "{0}~{1}-{2}".format(big_version, version_stage, small_version)
+            version = "{0}-{1}".format(big_version, small_version)
         
         return version
 
@@ -155,15 +155,12 @@ def parse_command_line(args):
                         required=True,
                         help="the directory of repository",
                         action="store")
-    parser.add_argument("--manifest-repo-dir",
-                        required=True,
-                        help="the directory of manifest repository",
-                        action="store")
 
     parser.add_argument("--is-official-release",
+                        type=bool,
                         default=False,
-                        help="This release if official",
-                        action="store_true")
+                        help="whether this release is official",
+                        action="store")
 
     parsed_args = parser.parse_args(args)
     return parsed_args
@@ -171,12 +168,12 @@ def parse_command_line(args):
 def main():
     # parse arguments
     args = parse_command_line(sys.argv[1:])
-    generator = VersionGenerator(args.repo_dir, args.manifest_repo_dir)
+    generator = VersionGenerator(args.repo_dir)
     try:
         version = generator.generate_package_version(args.is_official_release)
         print version
     except Exception, e:
-        logging.error("Failed to generate version for {0} due to {1}\n Exiting now".format(args.repo_dir, e))
+        common.logging.error("Failed to generate version for {0} due to {1}\n Exiting now".format(args.repo_dir, e))
         sys.exit(1)
 
 if __name__ == "__main__":
