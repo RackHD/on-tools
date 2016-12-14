27f07c1de6c0898a8a95e767fee355c0e186015b
sunnyzhang <sunny.zhang@emc.com>
2016-12-14 01:11:53 -0500

    To address Brian's comment

--- index-migration/indexMigration.py
+++ index-migration/indexMigration.py
@@ -1,29 +1,17 @@
 import subprocess
-import time
-
-def check_directory(filename):
-    """
-    To verify if filename is in current directory
-    """
-    ls_out=subprocess.Popen(['ls'], stdout=subprocess.PIPE)
-    directories = ls_out.stdout.read()
-    return filename in directories
 
 def backup_db():
     """
     To backup mongo databases and save it to a directory called dump.bak
     """
-    if check_directory("dump"):
-       subprocess.Popen(['rm -rf dump*'], shell=True)
-       time.sleep(1)
-       assert not check_directory("dump"), "The dump file is not deleted successfully"
-
-    subprocess.Popen(['mongodump'], stdout=subprocess.PIPE)
-    time.sleep(3)
-    subprocess.Popen(['cp', '-rf', 'dump', 'dump.bak'], stdout=subprocess.PIPE)
-    ls_out=subprocess.Popen(['ls'], stdout=subprocess.PIPE)
-    directories = ls_out.stdout.read()
-    assert check_directory("dump.bak"), "The mongo DB is NOT bakcup successfully"
+    try:
+        subprocess.check_output(['ls dump'], shell=True);
+        subprocess.check_call(['rm -rf dump*'], shell=True)
+    except CalledProcessError:
+        pass
+    subprocess.check_call(['mongodump'])
+    subprocess.check_call(['cp', '-rf', 'dump', 'dump.bak'])
+    subprocess.check_output(['ls dump.bak'], shell=True);
 
 def get_collection():
     """
@@ -45,22 +33,27 @@ def drop_indexes():
        drop_index_output =  drop_index_result.stdout.read()
        for error in errors:
           assert error not in drop_index_output, "Error is found when dropping mongo database indexes from collection %s" % collection
-       break
 
-print "Warning! Execute this script will drop all indexes from mongo databases!\nYet the script will help you backup all mongo databases\
- in a directory called dump.save under your working directory.\nIf you were to restore your mongoDB after executing this script, \
-simpleyly run 'mongorestore dump.bak'.Continue running the script(Yes|No)?"
+def main():
+    """
+    To execute the index migration
+    """
+    print "Warning! Execute this script will drop all indexes from mongo databases!\nYet the script will help you backup all mongo databases\
+in a directory called dump.save under your working directory.\nIf you were to restore your mongoDB after executing this script, \
+simpleyly run 'mongorestore dump.bak'.\nContinue running the script(Yes|No)?"
+    for attemp in range(3):
+        user_input = raw_input()
+        if user_input not in ['Yes', 'No']:
+            print "Incorrect input, please input Yes or No"
+            if attemp == 2:  print "You've wasted 3 attempts to give a correct input, please re-run the script!"
+        elif user_input == "No":
+            print "The script is NOT executed!"
+            break
+        else:
+            backup_db()
+            drop_indexes()
+            print "The indexes are dropped successfully!"
+            break
 
-for attemp in range(3):
-    user_input = raw_input()
-    if user_input not in ['Yes', 'No']:
-        print "Incorrect input, please input Yes or No"
-        if attemp == 2:  print "You've wasted 3 attempts to give a correct input, please re-run the script!"
-    elif user_input == "No":
-        print "The script is NOT executed!"
-        break
-    else:
-        backup_db()
-        drop_indexes()
-        print "The indexes are dropped successfully!"
-        break
+if __name__ == '__main__':
+    main()
